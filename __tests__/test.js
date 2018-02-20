fa81968605285032087bbf1765ab4aa1c02f5452
jpsierens <jpsierens1988@gmail.com>
2018-02-19 21:48:33 -0600

    fighting with tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 import TagsInput from '../build';
@@ -12,6 +12,14 @@ describe('<TagsInput />', () => {
 		expect(shallow(<TagsInput />).find('.react-tags-input').length).toBe(1);
 	});
 
+	it('should create a tag when the user enters text', () => {
+		const onInput = () => false;
+		const onRemove = () => false;
+		const tagsInput = mount(<TagsInput onInput={onInput} onRemove={onRemove} />);
+		console.log(tagsInput.state())
+		// tagsInput.setState({  })
+	});
+
 	it('should render clear cta', () => {
 		const tagsInput = shallow(<TagsInput onClear={() => 0} />);
 		expect(tagsInput.find('.clear-all').length).toBe(1);
@@ -20,5 +28,10 @@ describe('<TagsInput />', () => {
 	it('should render clear cta\'s default text', () => {
 		const tagsInput = shallow(<TagsInput onClear={() => 0} />);
 		expect(tagsInput.find('.clear-all').text()).toBe('Clear all');
+	});
+
+	it('should render clear cta\'s custom text', () => {
+		const tagsInput = shallow(<TagsInput clearAllText="custom" onClear={() => 0} />);
+		expect(tagsInput.find('.clear-all').text()).toBe('custom');
 	});
 });
